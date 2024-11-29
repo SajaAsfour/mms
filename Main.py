45f5cba3abf5f07d08e77afcffba2dad536a1ee1
SajaAsfour <sajanazih2004@gmail.com>
2024-11-30 00:55:25 +0200

    first

---
+++ Main.py
@@ -0,0 +1,46 @@
+import API
+import heapq
+import sys
+import time
+from constants import *
+from Algorithms.Floodfill import FloodFill
+from Algorithms.RightHand import RightHandRule
+from Algorithms.LeftHand import LeftHandRule
+
+
+def color_center():
+    API.setColor(CENTER, CENTER, "G")
+    API.setColor(CENTER - 1, CENTER, "G")
+    API.setColor(CENTER - 1, CENTER - 1, "G")
+    API.setColor(CENTER, CENTER - 1, "G")
+
+
+def main():
+
+    API.log("Running...")
+    API.setColor(0, 0, "G")
+    API.setText(0, 0, "start")
+
+    color_center()
+
+
+    API.setText(CENTER, CENTER, "Goal")
+
+
+    #floodfill = FloodFill()
+    #leftHandRule = LeftHandRule()
+    #rightHandRule = RightHandRule()
+
+
+    #floodfill.execute()
+    #leftHandRule.execute()
+    #rightHandRule.execute()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
